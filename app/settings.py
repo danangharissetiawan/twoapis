7768f26cf4d292a356fe5cee2f08877947f57ea6
danangharissetiawan <mr.danangharissetiawan@gmail.com>
2022-11-03 12:25:48 +0700

    update test path

--- app/settings.py
+++ app/settings.py
@@ -16,7 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+if 'test' in sys.argv:
+    SECRET_KEY = "test203200vbhdifhodifdifoiueiu03949"
+else:
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
